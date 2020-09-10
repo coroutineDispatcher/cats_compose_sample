ab4caf647a233d8fa6b45c089c53bfc2392a4d83
Stavro Xhardha <=>
2020-09-10 23:47:07 +0200

    Add back button in top app bar

--- app/src/main/java/com/coroutinedispatcher/cats/MainActivity.kt
+++ app/src/main/java/com/coroutinedispatcher/cats/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Text
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.vectorResource
 import com.coroutinedispatcher.cats.ui.CatsTheme
 import com.coroutinedispatcher.cats.ui.home.HomeScreen
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -49,13 +49,21 @@ fun CatsApp(
 ) {
     val currentState = navigationViewModel.state.collectAsState()
     Scaffold(
+        backgroundColor = Color.White,
         topBar = {
-            TopAppBar(
-                title = { Text(text = "Cats <3") }
-            )
+            when (currentState.value) {
+                NavigationViewModel.NavigationState.Home -> TopAppBar(title = { Text(text = "Cats <3") })
+                NavigationViewModel.NavigationState.ImageDetails -> TopAppBarWithBackButton(
+                    currentState.value
+                ) {
+                    navigationViewModel.goBack()
+                }
+            }
         },
         bodyContent = {
-            Crossfade(current = currentState) { screenState ->
+            Crossfade(
+                current = currentState
+            ) { screenState ->
                 when (screenState.value) {
                     NavigationViewModel.NavigationState.Home -> {
                         HomeScreen(
@@ -64,9 +72,27 @@ fun CatsApp(
                         )
                     }
                     NavigationViewModel.NavigationState.ImageDetails -> {
+                        Text(text = "Some text")
                     }
                 }
             }
+        }
+    )
+}
+
+@ExperimentalCoroutinesApi
+@Composable
+fun TopAppBarWithBackButton(
+    navigationState: NavigationViewModel.NavigationState,
+    action: () -> Unit
+) {
+    TopAppBar(
+        title = { Text(text = "Cats <3") },
+        navigationIcon = {
+            if (navigationState != NavigationViewModel.NavigationState.Home)
+                IconButton(onClick = { action() }) {
+                    Icon(vectorResource(R.drawable.ic_baseline_arrow_white_24))
+                }
         }
     )
 }
