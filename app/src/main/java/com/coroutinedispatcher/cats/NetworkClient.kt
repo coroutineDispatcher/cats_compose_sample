98e3900272bf114fc0ef98de9aa04567fd8b3d1e
Stavro Xhardha <=>
2020-09-10 23:47:39 +0200

    Add image request method

--- app/src/main/java/com/coroutinedispatcher/cats/NetworkClient.kt
+++ app/src/main/java/com/coroutinedispatcher/cats/NetworkClient.kt
@@ -1,13 +1,20 @@
 package com.coroutinedispatcher.cats
 
 import com.coroutinedispatcher.cats.model.Breed
+import com.coroutinedispatcher.cats.model.ImageResult
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NetworkClient {
     @GET("breeds")
-    suspend fun getPosts(
+    suspend fun fetchBreeds(
         @Query("attach_breed")
         attachBreed: Int = 0
     ): List<Breed>
+
+    @GET("images/search")
+    suspend fun fetchImage(
+        @Query("breed_id")
+        breedId: String
+    ): List<ImageResult>
 }
